9ae178daacad6ea93aced4ce4cb2bc7b97d0c2f6
kylebunting <kyle.bunting@gmail.com>
2018-01-20 14:29:25 -0500

    Created dummy test case.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    'Hello, world!'
+    , div);
 });
