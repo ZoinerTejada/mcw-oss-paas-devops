6609cec12e5a0b3dd2a61322193dd0ba044218f5
kylebunting <kyle.bunting@gmail.com>
2018-01-20 15:56:52 -0500

    Modified Order model to reference _id of other models

--- models/Order.js
+++ models/Order.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 // Create the Order schema, showing the shape of the order database entity.
-var orderSchema = new mongoose.Schema({
-    customerId: { type: String, required: true },
-    planId: { type: String, required: true },
+var orderSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    planId: { type: Schema.Types.ObjectId, required: true, ref: 'Plan' },
     dayToProcess: { type: Number, required: true },
     orderDate: { type: Date, default: Date.now, required: true },
     notificationSent: { type: Boolean, default: false, required: true }
