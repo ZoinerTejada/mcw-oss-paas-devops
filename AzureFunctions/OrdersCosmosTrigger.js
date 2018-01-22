672808a4336f319676ad57b4d886d9b294c98dc3
kylebunting <kyle.bunting@gmail.com>
2018-01-22 18:45:09 -0500

    Added Azure Function code

---
+++ AzureFunctions/OrdersCosmosTrigger.js
@@ -0,0 +1,30 @@
+/*
+	OrdersCosmosTrigger function code
+	* Trigger: Azure Cosmos DB on newOrders into the orders collection.
+	* Output: Azure Storage Queue - orderqueue
+	
+	Triggers whenever a document in the orders collection is inserted or updated.
+	By checking processed on the document, we only send unprocessed orders to the queue,
+	so when the next Function updates the processed value to true, it won't be requeued.
+*/
+module.exports = function (context, newOrders) {
+	for(var i=0; i < newOrders.length; i++) {
+		var newOrder = newOrders[i];
+		var orderId = newOrder['$v']['id']['$v'];
+		var userId = newOrder['$v']['userId']['$v'];
+		var sendNotification = newOrder['$v']['sendNotification']['$v'];
+		var processed = newOrder['$v']['processed']['$v'];
+		if (!processed) {
+			context.log('Sent to orderqueue: order id ', orderId);
+			context.bindings.outputQueue = {
+				orderId: orderId,
+				userId: userId,
+				sendNotification: sendNotification
+			};
+		}
+		else {
+			context.log('Already processed, not sent to orderqueue: order id ', orderId);
+		}
+    }
+	context.done();
+}
