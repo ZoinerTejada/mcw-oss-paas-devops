196fafd8ef41cbfb1e990eef0b546fee7e251068
kylebunting <kyle.bunting@gmail.com>
2018-04-09 10:58:10 -0400

    Adding Linux VM ARM template.

---
+++ data/mongo-export.sh
@@ -0,0 +1,3 @@
+mongoexport --db best-for-you-organics --collection plans --out ~/MongoExport/plans.json
+mongoexport --db best-for-you-organics --collection users --out ~/MongoExport/users.json
+mongoexport --db best-for-you-organics --collection orders --out ~/MongoExport/orders.json
--- setup/linuxlabvmconfig.sh
+++ setup/linuxlabvmconfig.sh
@@ -1,9 +1,14 @@
 export DEBIAN_FRONTEND=noninteractive
 #Install LXDE lxde.org and xrdp - (make sure to open 3389 on the NSG of the azure vm)
 apt-get update
-apt-get install lxde -y
-apt-get install xrdp -y
+apt-get install -y lxde
+apt-get install -y xrdp
 /etc/init.d/xrdp start
+
+#Prepare XWindows System
+sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
+sudo touch ~/.xsession
+echo "startlxde" > ~/.xsession
 
 #installing visual studio code which can be launched from accessories
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -11,18 +16,13 @@ mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sed -i 's/BIG-REQUESTS/_IG-REQUESTS/' /usr/lib/x86_64-linux-gnu/libxcb.so.1
 apt-get update
-apt-get install code
-
-#Prepare XWindows System
-sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
-wget https://opsgilityweb.blob.core.windows.net/test/xsession
-mv xsession /home/demouser/.xsession
+apt-get install -y code
 
 #install the Azure CLI using instructions from Azure.com
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
      sudo tee /etc/apt/sources.list.d/azure-cli.list
 apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
-apt-get install apt-transport-https -y
+apt-get install -y apt-transport-https
 apt-get update && sudo apt-get install -y azure-cli
 
 #install node.js and npm
@@ -30,7 +30,7 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
 
 #install MongoDB Community Edition
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 apt-get update && sudo apt-get install -y mongodb-org
 sudo service mongod start
@@ -46,10 +46,3 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 apt-get update
 apt-get install -y docker-ce
-
-#download, extract, and create shorcut for Azure Storage Explorer
-#mkdir /usr/share/storageexplorer
-#wget https://go.microsoft.com/fwlink/?LinkId=722418 -O /usr/share/storageexplorer/StorageExplorer.tar.gz
-#tar -xvzf /usr/share/storageexplorer/StorageExplorer.tar.gz -C /usr/share/storageexplorer
-#wget https://raw.githubusercontent.com/opsgility/lab-support-public/master/script-extensions/storageexplorer.desktop -O /usr/share/applications/storageexplorer.desktop
-#chmod a+x /usr/share/applications/storageexplorer.desktop
